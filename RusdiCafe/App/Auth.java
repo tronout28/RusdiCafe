e2fcf12c35ad653c6d0ba7a75d79ba79b599954e
jezettx <mcpeyodha@gmail.com>
2025-12-30 11:57:46 +0700

    Fix Auth If User and Pass

--- RusdiCafe/App/Auth.java
+++ RusdiCafe/App/Auth.java
@@ -6,22 +6,22 @@ public class Auth {
 
     private final String USERNAME = "admin";
     private final String PASSWORD = "12345";
-
     private Scanner scan = new Scanner(System.in);
-
     public boolean login() {
-        System.out.println("=== LOGIN ===");
-        System.out.print("Username : ");
-        String user = scan.nextLine();
+        do {
+            System.out.println("=== LOGIN ===");
+            System.out.print("Username : ");
+            String user = scan.nextLine();
+            
+            System.out.print("Password : ");
+            String pass = scan.nextLine();
 
-        System.out.print("Password : ");
-        String pass = scan.nextLine();
-
-        if (user.equals(USERNAME) && pass.equals(PASSWORD)) {
-            return true;
-        } else {
-            System.out.println("Username atau Password salah!");
-            return false; 
-        }
+            if (user.equals(USERNAME) && pass.equals(PASSWORD)) {
+                return true;
+            } else {
+                System.out.println("Username atau Password salah!");
+            }
+            System.out.println();
+        } while (true);
     }
 }
